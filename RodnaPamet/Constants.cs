f9702a289c180c74433be01b8927dd096c00f776
Стефан Костадинов <stefan@yobul.com>
2021-12-27 15:31:24 +0200

    Beta version setup

--- RodnaPamet/Constants.cs
+++ RodnaPamet/Constants.cs
@@ -8,7 +8,7 @@ namespace RodnaPamet
     public static class Constants
     {
         // The iOS simulator can connect to localhost. However, Android emulators must use the 10.0.2.2 special alias to your host loopback interface.
-        public static string BaseAddress = "https://api.rodnapamet.bg";
+        public static string BaseAddress = "https://devapi.rodnapamet.bg";
         public static string UsersUrl = BaseAddress + "/users";
         public static string AuthUrl = BaseAddress + "/auth";
 
